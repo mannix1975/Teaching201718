0a86e386dc959f89fe6fb14d17f11c96b051f971
Mark Foley <mark@markfoley.info>
2017-09-12 00:00:50 +0100

    latest

---
+++ Python/test1.py
@@ -0,0 +1,23 @@
+print("Hello World")
+
+a_very_long_variable_name = 42
+
+print(a_very_long_variable_name)
+
+a_piece_of_text = "random stuff"
+
+a_piece_of_text.capitalize()
+
+an_int = 22
+
+import math
+
+math.pi
+
+a = 2
+b = 3
+
+if a == b:
+    print("they are equal")
+else:
+    print("they are not equal")
